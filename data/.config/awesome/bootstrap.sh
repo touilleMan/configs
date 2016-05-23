1b50d7349bd40d2b4ca3b1dea9e42c2fd73cb465
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2016-05-23 16:21:59 +0200

    Add init script

---
+++ data/.config/awesome/bootstrap.sh
@@ -0,0 +1,4 @@
+ #!/bin/sh
+
+dex -a -e Awesome
+xrate r rate 200
---
+++ init.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python3
+
+from subprocess import call
+from pathlib import Path
+import os
+
+SRC_DIR=Path(os.path.abspath(os.path.dirname(__file__) + '/data'))
+DEST_DIR=Path(os.environ['HOME'])
+
+
+def install_package(package):
+    if hasattr(package, '__iter__'):
+        package = ' '.join(package)
+    cmd = 'sudo apt-get install %s' % package
+    print('=> install %s' % package)
+    ret = runcmd(cmd)
+    assert ret == 0, 'Cannot install %s' % package
+
+
+def runcmd(cmd):
+    return call(cmd, shell=True)
+
+
+def install_conf(conf):
+    p = Path(conf)
+    source = SRC_DIR.joinpath(p)
+    assert source.exists(), '%s is not a valid path' % source
+    dest = DEST_DIR.joinpath(p)
+    if dest.exists():
+        # Make sure the existing dir has is what we wanted to install
+        assert dest.is_symlink() and dest.resolve() == source, '%s already exists but is not a symlink on %s' % (dest, source)
+    else:
+        dest.symlink_to(source)
+
+
+class App:
+    def install(self):
+        print('Installing %s' % self.__class__.__name__)
+        self._install()
+
+    def _install(self):
+        raise NotImplementedError
+
+
+# install good stuff
+
+print(' * zsh', end='', flush=True)
+if runcmd('which zsh > /dev/null') != 0:
+    install_package('zsh')
+if not DEST_DIR.joinpath('.oh-my-zsh').exists():
+    runcmd('sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"')
+install_conf('.oh-my-zsh/custom/perso.zsh')
+print(' ✓')
+
+print(' * awesome', end='', flush=True)
+if runcmd('which awesome > /dev/null') != 0:
+    install('awesome')
+install_conf('.config/awesome')
+print(' ✓')
+
+print(' * python shell conf', end='', flush=True)
+install_conf('.config/python_shell_conf.py')
+print(' ✓')
+
+print(' * scripts dir', end='', flush=True)
+install_conf('.scripts')
+print(' ✓')
+
+print("""
+*** TODO ***
+ * Dropbox https://www.dropbox.com/install
+ * what else ? ;-)
+""")

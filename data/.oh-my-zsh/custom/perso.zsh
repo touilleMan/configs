100d6c2190f4aca8e90380e6aa96b94edf239a8d
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2016-05-24 12:59:54 +0200

    Clean oh-my-zsh (end)

---
+++ data/.oh-my-zsh/custom/perso.zsh
@@ -0,0 +1,47 @@
+# Put your fun stuff here.
+eval `gnome-keyring-daemon --start`
+
+alias ul='ls --color=auto --classify -lh'
+alias ua='ls --color=auto --classify -a'
+alias u='ls --color=auto --classify'
+alias p='cd'
+
+alias -s pdf="okular "
+alias -s txt="nano "
+alias -s avi="vlc "
+alias -s mkv="vlc "
+
+alias clean='find . -name "*~" -delete -print'
+alias prince='dosbox -c "cd PRINCE" -c "PRINCE.EXE"'
+alias prince2='dosbox -c "cd PRINCOP2" -c "PRINCE.EXE"'
+alias tie='dosbox -c "cd TIE" -c "TIE.EXE"'
+alias duke='cd ~/.eduke32/ && ./eduke32'
+
+alias fullgit='git fetch && git stash && git rebase && git stash pop'
+
+alias soulsave=$HOME/.soulSaver/soulsave.sh
+alias souladd=$HOME/.soulSaver/souladd.sh
+
+alias movget=$HOME/.scripts/movget.sh
+
+alias todo2='todo2 --file=$HOME/.config/todo2'
+
+alias tc='truecrypt ~/Documents/.emmanuel_crypted_big.tc ~/tc'
+alias tck='truecrypt -d && touch ~/Documents/.emmanuel_crypted_big.tc'
+
+alias httpserver='twistd -no web --path=. -p 8000'
+
+# add custom script to program list
+export PATH="$PATH:$HOME/.scripts"
+# export PYTHONPATH=/usr/local/lib/python2.7/dist-packages
+export GOPATH=$HOME/.gopath
+export PATH=$PATH:$HOME/.config/delation
+
+### Added by the Heroku Toolbelt
+export PATH="$PATH:/usr/local/heroku/bin"
+export PATH="$PATH:$HOME/.config/npm/bin"
+
+### Awesome sam&max stuff !
+export PYTHONSTARTUP=$HOME/.config/python_shell_conf.py
+
+export PATH=$HOME/.config/bin:$PATH

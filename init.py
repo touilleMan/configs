2177407a0de410109bbba4838545a1ceb7c258dd
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2019-04-04 12:09:18 +0200

    Improve install script

--- init.py
+++ init.py
@@ -3,14 +3,23 @@
 from subprocess import call
 from pathlib import Path
 import os
+import sys
 
 
-SRC_DIR=Path(os.path.abspath(os.path.dirname(__file__) + '/data'))
+SRC_DIR=Path(os.path.abspath(os.path.dirname(__file__)) + '/data')
 DEST_DIR=Path(os.environ['HOME'])
+
+DRY_RUN = '--dry' in sys.argv[1:]
+if DRY_RUN:
+    sys.argv = [x for x in sys.argv if x != '--dry']
 
 
 def is_installed(to_check):
-    return runcmd('which %s > /dev/null' % to_check) == 0
+    if DRY_RUN:
+        return False
+
+    else:
+        return runcmd('which %s > /dev/null' % to_check) == 0
 
 
 def install_package(package, to_check=None):
@@ -19,13 +28,18 @@ def install_package(package, to_check=None):
     if isinstance(package, (list, tuple)):
         package = ' '.join(package)
     cmd = 'sudo apt install -y %s' % package
-    print('=> install %s' % package)
+    if not DRY_RUN:
+        print('=> install %s' % package)
     ret = runcmd(cmd)
     assert ret == 0, 'Cannot install %s' % package
 
 
 def runcmd(cmd):
-    return call(cmd, shell=True)
+    if DRY_RUN:
+        print(cmd)
+        return 0
+    else:
+        return call(cmd, shell=True)
 
 
 def install_conf(conf):
@@ -33,17 +47,16 @@ def install_conf(conf):
     source = SRC_DIR.joinpath(p)
     assert source.exists(), '%s is not a valid path' % source
     dest = DEST_DIR.joinpath(p)
+
+    if DRY_RUN:
+        print('ln -s %s %s' % (source, dest))
+        return 0
+
     if dest.exists():
         # Make sure the existing dir has is what we wanted to install
         assert dest.is_symlink() and dest.resolve() == source, '%s already exists but is not a symlink on %s' % (dest, source)
     else:
         dest.symlink_to(source)
-
-
-def desktop_enable_app(appname):
-    app = '/etc/xdg/autostart/%s.desktop' % appname
-    if runcmd("grep '^OnlyShowIn=.*Awesome;$' %s" % app) != 0:
-        runcmd("sudo sed  -is 's/OnlyShowIn=Awesome;/OnlyShowIn=/' %s" % app)
 
 
 def manual_install(name, site, to_check=None):
@@ -52,50 +65,70 @@ def manual_install(name, site, to_check=None):
     runcmd('xdg-open %s && echo "Install %s and type ENTER to continue" && read _' % (site, name))
 
 
-# install good stuff
+def main():
+    cmds = {x.__name__: x for x in [all, base, scripts, fish, pyenv, sublime_text]}
+    usage = "%s [%s] [--dry]" % (sys.argv[0], '|'.join(cmds.keys()))
+    if len(sys.argv) == 1:
+        raise SystemExit(usage)
 
-print(' * basics', end='', flush=True)
-install_package('curl')
-install_package('python-pip', to_check='pip')
-install_package('python3-pip', to_check='pip3')
-runcmd("sudo pip install -U pip")
-runcmd("sudo pip3 install -U pip3")
-runcmd("sudo pip3 install virtualenv tox flake8 pylint")
-install_package('curl')
-print(' ✓')
+    cmd_name = sys.argv[1]
+    try:
+        cmd = cmds[cmd_name]
+    except KeyError:
+        raise SystemExit(usage)
+    cmd()
 
-print(' * zsh', end='', flush=True)
-install_package('zsh')
-if not DEST_DIR.joinpath('.oh-my-zsh').exists():
-    runcmd('sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"')
 
-install_conf('.oh-my-zsh/custom/perso.zsh')
-print(' ✓')
+def all():
+    base()
+    scripts()
+    sublime_text()
 
-print(' * awesome', end='', flush=True)
-install_package('awesome')
-desktop_enable_app('gnome-keyring-*')
-desktop_enable_app('gnome-sound-panel')
-desktop_enable_app('gnome-power-panel')
-install_conf('.config/awesome')
-print(' ✓')
 
-print(' * python shell conf', end='', flush=True)
-install_conf('.config/python_shell_conf.py')
-print(' ✓')
+def base():
+    if not DRY_RUN:
+        print(' * basics', end='', flush=True)
+    install_package('curl')
+    install_package('fish')
+    install_package('python3')
+    if not DRY_RUN:
+        print(' ✓')
 
-print(' * scripts dir', end='', flush=True)
-install_conf('.scripts')
-print(' ✓')
 
-runcmd('git config --global user.email "emmanuel.leblond@gmail.com"')
-runcmd('git config --global user.name "Emmanuel Leblond"')
+def pyenv():
+    if not DRY_RUN:
+        print(' * pyenv', end='', flush=True)
+    if not is_installed('pyenv'):
+        runcmd("curl https://pyenv.run | bash")
+    if not DRY_RUN:
+        print(' ✓')
 
-print(' * sublime text', end='', flush=True)
-manual_install('sublime text', 'https://www.sublimetext.com/3', 'subl')
-runcmd('mkdir -p %s/.config/sublime-text-3/Packages/' % DEST_DIR)
-install_conf('.config/sublime-text-3/Packages/User')
-runcmd('wget https://packagecontrol.io/Package%%20Control.sublime-package -O %s/.config/sublime-text-3/Installed\ Packages/Package\ Control.sublime-package' % DEST_DIR)
-print(' ✓')
+def scripts():
+    if not DRY_RUN:
+        print(' * scripts dir', end='', flush=True)
+    install_conf('.scripts')
+    if not DRY_RUN:
+        print(' ✓')
 
-manual_install('Dropbox', 'https://www.dropbox.com/install', 'dropbox')
+
+def sublime_text():
+    if not DRY_RUN:
+        print(' * sublime text', end='', flush=True)
+    manual_install('sublime text', 'https://www.sublimetext.com/3', 'subl')
+    runcmd('mkdir -p %s/.config/sublime-text-3/Packages/' % DEST_DIR)
+    install_conf('.config/sublime-text-3/Packages/User')
+    runcmd('wget https://packagecontrol.io/Package%%20Control.sublime-package -O %s/.config/sublime-text-3/Installed\ Packages/Package\ Control.sublime-package' % DEST_DIR)
+    if not DRY_RUN:
+        print(' ✓')
+
+
+def fish():
+    if not DRY_RUN:
+        print(' * fish', end='', flush=True)
+    install_conf('.config/fish/config.fish')
+    if not DRY_RUN:
+        print(' ✓')
+
+
+if __name__ == '__main__':
+    main()
